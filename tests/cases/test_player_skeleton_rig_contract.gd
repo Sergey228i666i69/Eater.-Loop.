c39026e3720d7f201b6258b0100012ebdfb38eae
Sergey228i666i69 <cgenerozov@gmail.com>
2026-06-24 18:24:16 +0700

    Add Andry empty-hand wrist sway

--- tests/cases/test_player_skeleton_rig_contract.gd
+++ tests/cases/test_player_skeleton_rig_contract.gd
@@ -142,6 +142,8 @@ const LIGHT_RUN_SAMPLE_TIMES: Array[float] = [0.0, 0.06875, 0.1375, 0.20625, 0.2
 const FOOT_CONTACT_GROUND_TOLERANCE := 12.0
 const IDLE_MIN_HAND_BREATH_RANGE := 0.009
 const IDLE_MAX_HAND_BREATH_RANGE := 0.02
+const IDLE_MIN_EMPTY_HAND_BREATH_RANGE := 0.008
+const IDLE_MAX_EMPTY_HAND_BREATH_RANGE := 0.012
 const WALK_MIN_HIPS_BOUNCE_RANGE := 2.5
 const WALK_MAX_HIPS_BOUNCE_RANGE := 3.5
 const WALK_MIN_LEG_SWING_RANGE := 0.055
@@ -156,6 +158,8 @@ const WALK_MIN_FOREARM_FOLLOW_RANGE := 0.035
 const WALK_MAX_FOREARM_FOLLOW_RANGE := 0.045
 const WALK_MIN_FLASHLIGHT_BOB_RANGE := 0.085
 const WALK_MAX_FLASHLIGHT_BOB_RANGE := 0.095
+const WALK_MIN_EMPTY_HAND_SWAY_RANGE := 0.03
+const WALK_MAX_EMPTY_HAND_SWAY_RANGE := 0.04
 const WALK_MIN_CHEST_COUNTER_RANGE := 0.03
 const WALK_MAX_CHEST_COUNTER_RANGE := 0.04
 const WALK_MIN_SPINE_SWAY_RANGE := 0.03
@@ -178,6 +182,8 @@ const LIGHT_RUN_MIN_FOREARM_FOLLOW_RANGE := 0.06
 const LIGHT_RUN_MAX_FOREARM_FOLLOW_RANGE := 0.07
 const LIGHT_RUN_MIN_FLASHLIGHT_BOB_RANGE := 0.17
 const LIGHT_RUN_MAX_FLASHLIGHT_BOB_RANGE := 0.19
+const LIGHT_RUN_MIN_EMPTY_HAND_SWAY_RANGE := 0.065
+const LIGHT_RUN_MAX_EMPTY_HAND_SWAY_RANGE := 0.075
 const LIGHT_RUN_MIN_CHEST_COUNTER_RANGE := 0.055
 const LIGHT_RUN_MAX_CHEST_COUNTER_RANGE := 0.065
 const LIGHT_RUN_MIN_SPINE_SWAY_RANGE := 0.04
@@ -216,6 +222,7 @@ const FRONT_UPPER_ARM_ROTATION_TRACK := NodePath("Skeleton2D/Hips/Spine/Chest/Fr
 const FRONT_FOREARM_ROTATION_TRACK := NodePath("Skeleton2D/Hips/Spine/Chest/FrontUpperArm/FrontForearm:rotation")
 const BACK_FOREARM_ROTATION_TRACK := NodePath("Skeleton2D/Hips/Spine/Chest/BackUpperArm/BackForearm:rotation")
 const FLASHLIGHT_MOUNT_ROTATION_TRACK := NodePath("Skeleton2D/Hips/Spine/Chest/FrontUpperArm/FrontForearm/FrontHand/FlashlightMount:rotation")
+const FRONT_HAND_EMPTY_VISUAL_ROTATION_TRACK := NodePath("Skeleton2D/Hips/Spine/Chest/FrontUpperArm/FrontForearm/FrontHand/VisualFrontHandEmpty:rotation")
 const FRONT_FOOT_POSITION_TRACK := NodePath("Skeleton2D/Hips/FrontThigh/FrontShin/FrontFoot:position")
 const BACK_FOOT_POSITION_TRACK := NodePath("Skeleton2D/Hips/BackThigh/BackShin/BackFoot:position")
 const FRONT_FOOT_ROTATION_TRACK := NodePath("Skeleton2D/Hips/FrontThigh/FrontShin/FrontFoot:rotation")
@@ -311,6 +318,7 @@ func _test_rig_scene_contract() -> void:
 					_assert_animation_track_value_range(animation, BACK_FOREARM_ROTATION_TRACK, IDLE_MIN_HAND_BREATH_RANGE, IDLE_MAX_HAND_BREATH_RANGE, String(animation_name))
 					_assert_animation_track_value_range(animation, FRONT_HAND_ROTATION_TRACK, IDLE_MIN_HAND_BREATH_RANGE, IDLE_MAX_HAND_BREATH_RANGE, String(animation_name))
 					_assert_animation_track_value_range(animation, BACK_HAND_ROTATION_TRACK, IDLE_MIN_HAND_BREATH_RANGE, IDLE_MAX_HAND_BREATH_RANGE, String(animation_name))
+					_assert_animation_track_value_range(animation, FRONT_HAND_EMPTY_VISUAL_ROTATION_TRACK, IDLE_MIN_EMPTY_HAND_BREATH_RANGE, IDLE_MAX_EMPTY_HAND_BREATH_RANGE, String(animation_name))
 				elif animation_name == &"walk":
 					_assert_animation_vector2_y_range(animation, HIPS_POSITION_TRACK, WALK_MIN_HIPS_BOUNCE_RANGE, WALK_MAX_HIPS_BOUNCE_RANGE, String(animation_name))
 					_assert_animation_track_value_range(animation, FRONT_THIGH_ROTATION_TRACK, WALK_MIN_LEG_SWING_RANGE, WALK_MAX_LEG_SWING_RANGE, String(animation_name))
@@ -324,6 +332,7 @@ func _test_rig_scene_contract() -> void:
 					_assert_animation_track_value_range(animation, FRONT_FOREARM_ROTATION_TRACK, WALK_MIN_FOREARM_FOLLOW_RANGE, WALK_MAX_FOREARM_FOLLOW_RANGE, String(animation_name))
 					_assert_animation_track_value_range(animation, BACK_FOREARM_ROTATION_TRACK, WALK_MIN_FOREARM_FOLLOW_RANGE, WALK_MAX_FOREARM_FOLLOW_RANGE, String(animation_name))
 					_assert_animation_track_value_range(animation, FLASHLIGHT_MOUNT_ROTATION_TRACK, WALK_MIN_FLASHLIGHT_BOB_RANGE, WALK_MAX_FLASHLIGHT_BOB_RANGE, String(animation_name))
+					_assert_animation_track_value_range(animation, FRONT_HAND_EMPTY_VISUAL_ROTATION_TRACK, WALK_MIN_EMPTY_HAND_SWAY_RANGE, WALK_MAX_EMPTY_HAND_SWAY_RANGE, String(animation_name))
 					_assert_animation_vector2_y_range(animation, FRONT_FOOT_POSITION_TRACK, WALK_MIN_FOOT_LIFT_RANGE, WALK_MAX_FOOT_LIFT_RANGE, String(animation_name))
 					_assert_animation_vector2_y_range(animation, BACK_FOOT_POSITION_TRACK, WALK_MIN_FOOT_LIFT_RANGE, WALK_MAX_FOOT_LIFT_RANGE, String(animation_name))
 					_assert_animation_track_value_range(animation, FRONT_FOOT_ROTATION_TRACK, WALK_MIN_FOOT_ROLL_RANGE, WALK_MAX_FOOT_ROLL_RANGE, String(animation_name))
@@ -347,6 +356,7 @@ func _test_rig_scene_contract() -> void:
 					_assert_animation_track_value_range(animation, FRONT_FOREARM_ROTATION_TRACK, LIGHT_RUN_MIN_FOREARM_FOLLOW_RANGE, LIGHT_RUN_MAX_FOREARM_FOLLOW_RANGE, String(animation_name))
 					_assert_animation_track_value_range(animation, BACK_FOREARM_ROTATION_TRACK, LIGHT_RUN_MIN_FOREARM_FOLLOW_RANGE, LIGHT_RUN_MAX_FOREARM_FOLLOW_RANGE, String(animation_name))
 					_assert_animation_track_value_range(animation, FLASHLIGHT_MOUNT_ROTATION_TRACK, LIGHT_RUN_MIN_FLASHLIGHT_BOB_RANGE, LIGHT_RUN_MAX_FLASHLIGHT_BOB_RANGE, String(animation_name))
+					_assert_animation_track_value_range(animation, FRONT_HAND_EMPTY_VISUAL_ROTATION_TRACK, LIGHT_RUN_MIN_EMPTY_HAND_SWAY_RANGE, LIGHT_RUN_MAX_EMPTY_HAND_SWAY_RANGE, String(animation_name))
 					_assert_animation_vector2_y_range(animation, FRONT_FOOT_POSITION_TRACK, LIGHT_RUN_MIN_FOOT_LIFT_RANGE, LIGHT_RUN_MAX_FOOT_LIFT_RANGE, String(animation_name))
 					_assert_animation_vector2_y_range(animation, BACK_FOOT_POSITION_TRACK, LIGHT_RUN_MIN_FOOT_LIFT_RANGE, LIGHT_RUN_MAX_FOOT_LIFT_RANGE, String(animation_name))
 					_assert_animation_vector2_x_range(animation, FRONT_FOOT_POSITION_TRACK, LIGHT_RUN_MIN_FRONT_FOOT_STRIDE_RANGE, LIGHT_RUN_MAX_FRONT_FOOT_STRIDE_RANGE, String(animation_name))
